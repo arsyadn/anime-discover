107314d954c89feee6ed6f44feb633db1c9685d0
arsyadn <arsyadnurzaky@gmail.com>
2026-01-11 13:25:48 +0700

    feat(pagination): change pagination limit

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_LIMIT = 15;
+export const DEFAULT_LIMIT = 10;
 
 export function getOffset(page: number, limit: number = DEFAULT_LIMIT): number {
   return Math.max(page - 1, 0) * limit;
